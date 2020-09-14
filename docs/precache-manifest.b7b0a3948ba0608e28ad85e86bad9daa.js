96a6f7a91f5d43902f9015f07b6d1cb84e92b559
Mohammed, Nawaz <nawaz.mohammed@tyson.com>
2020-09-15 02:11:14 +0530

    adding docs

---
+++ docs/precache-manifest.b7b0a3948ba0608e28ad85e86bad9daa.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b90bcb90ad61ac5b3d8da7ced65a9413",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5086a5eeff21cd48086",
+    "url": "/static/css/2.86b33428.chunk.css"
+  },
+  {
+    "revision": "6ad0eaf353351c8d89f3",
+    "url": "/static/css/main.6d944a1e.chunk.css"
+  },
+  {
+    "revision": "c5086a5eeff21cd48086",
+    "url": "/static/js/2.e76da6de.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.e76da6de.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6ad0eaf353351c8d89f3",
+    "url": "/static/js/main.8fc83283.chunk.js"
+  },
+  {
+    "revision": "9d41eeb9a8c8123b7176",
+    "url": "/static/js/runtime-main.294d7aae.js"
+  },
+  {
+    "revision": "70e45f4403a2ffacf06c7a66910bd527",
+    "url": "/static/media/coming_soon.70e45f44.gif"
+  },
+  {
+    "revision": "0775a7d601bdba842e345bf231a565c7",
+    "url": "/static/media/no_data.0775a7d6.jpg"
+  }
+]);
